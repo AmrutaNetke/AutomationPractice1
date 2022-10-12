5b06be5ca913599efd9842229f24e8f52c1081ee
AmrutaNetke <netkeamruta094@gmail.com>
2022-10-12 17:17:04 +0530

    Updated class1

--- AutomationPractice1/src/Package1/Class1.java
+++ AutomationPractice1/src/Package1/Class1.java
@@ -3,5 +3,6 @@ package Package1;
 public class Class1 {
 public static void main(String[] args) {
 	System.out.println("This is world of java");
+	System.out.println("You are always welcome here... ");
 }
 }
