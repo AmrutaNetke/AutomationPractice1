ed68c8fcda5b617b92a84622a1836b4ef5d8968f
AmrutaNetke <netkeamruta094@gmail.com>
2022-10-12 17:42:34 +0530

    Updated class1

--- AutomationPractice1/src/Package1/Class1.java
+++ AutomationPractice1/src/Package1/Class1.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("This is world of java");
 	System.out.println("You are always welcome here... ");
 	System.out.println("You need to work hard");
+	System.out.println("Line 4");
 }
 }
