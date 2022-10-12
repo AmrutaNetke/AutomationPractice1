f5f3b53cffc9bc47ed6b9ea141cdaa7971c4d0bc
AmrutaNetke <netkeamruta094@gmail.com>
2022-10-12 17:38:38 +0530

    Commited to local repo

--- AutomationPractice1/src/Package1/Class1.java
+++ AutomationPractice1/src/Package1/Class1.java
@@ -4,5 +4,6 @@ public class Class1 {
 public static void main(String[] args) {
 	System.out.println("This is world of java");
 	System.out.println("You are always welcome here... ");
+	System.out.println("You need to work hard");
 }
 }
