90678d8a290ca01f6b1b7811cbe9064b09fc19a7
AmrutaNetke <netkeamruta094@gmail.com>
2022-10-14 11:22:31 +0530

    Udpdated Branch1

--- AutomationPractice1/src/Package1/Class1.java
+++ AutomationPractice1/src/Package1/Class1.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("You need to work hard");
 	System.out.println("Line 4");
 	System.out.println("Line 5");
+	System.out.println("Added to Branch1");
 }
 }
